ea86f5f984adabe5695f3189d8f54dd33a973395
KatieStocker <stocker_katie@aol.co.uk>
2022-04-20 15:35:49 +0100

    added code to pass the failing test - the correct number of className can be found

--- frontend/src/Components/Home.jsx
+++ frontend/src/Components/Home.jsx
@@ -1,11 +1,29 @@
+import { testCCData } from '../test/testData/sampleCommunityCentreData';
 
 const Home = () => {
+
+    const data = testCCData[0].CommunityCentre;
+
+    const populateBundles = () => {
+        const displayBundles = data.map(currentBundle => {
+            return (
+                <div className="bundle-location" key={currentBundle._id}>
+                    {currentBundle.name}
+                </div>
+            );
+        });
+        return displayBundles;
+    }
+
 
     return (
         <>
             <h1>
                 Community Centre Checklist
             </h1>
+            <div className="container" key="bundles">
+                {populateBundles()}
+            </div>
         </>
     );
 };
