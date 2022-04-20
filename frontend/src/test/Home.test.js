86d277b5025f1e56ae504e0cd50f62159d252d7b
KatieStocker <stocker_katie@aol.co.uk>
2022-04-20 15:16:32 +0100

    added a failing test to count the number of divs present on the home page

--- frontend/src/test/Home.test.js
+++ frontend/src/test/Home.test.js
@@ -1,5 +1,6 @@
 import { render, screen } from '@testing-library/react';
 import Home from '../Components/Home.jsx';
+import { testCCData } from './testData/sampleCommunityCentreData';
 
 describe(`Home test suite`, () => {
 
@@ -9,5 +10,11 @@ describe(`Home test suite`, () => {
 
     test(`it should render a heading with the text 'Community Centre Checklist'`, () => {
         expect(screen.getByText('Community Centre Checklist')).toBeInTheDocument();
+    });
+
+    test(`it should render 7 divs with class name '.bundle-location' to coincide with the number of bundles in the data`, () => {
+        const { container } = render(<Home />);
+        const count = container.querySelectorAll(`.bundle-location`).length;
+        expect(count).toBe(testCCData[0].CommunityCentre.length);
     });
 });
